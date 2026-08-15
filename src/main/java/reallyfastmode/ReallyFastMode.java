c6972bb7ec10bc70c8b116439ca57efffca30899
petitioner0 <petitioner4@gmail.com>
2026-08-15 23:20:11 +0800

    initial setting

---
+++ src/main/java/reallyfastmode/ReallyFastMode.java
@@ -0,0 +1,13 @@
+package reallyfastmode;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+
+@SpireInitializer
+public final class ReallyFastMode {
+    private ReallyFastMode() {
+    }
+
+    public static void initialize() {
+    }
+}
