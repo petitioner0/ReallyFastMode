0a426643937b2bf68d0f906f04e832c816a13741
petitioner0 <petitioner4@gmail.com>
2026-09-06 02:37:47 +0800

    Update ProtocolSlots.java

--- src/main/java/reallyfastmode/protocol/ProtocolSlots.java
+++ src/main/java/reallyfastmode/protocol/ProtocolSlots.java
@@ -12,16 +12,20 @@ public final class ProtocolSlots {
     public static final int GAME = 0;
     public static final int PLAYER = 1;
     public static final int COMBAT = 2;
-    public static final int CARD = 3;
-    public static final int MONSTER = 4;
-    public static final int POTION = 5;
-    public static final int SELECTION = 6;
-    public static final int MAP = 7;
-    public static final int REWARD = 8;
-    public static final int EVENT = 9;
-    public static final int SHOP = 10;
-    public static final int REST = 11;
-    public static final int SIZE = 12;
+    public static final int DECK_CARD = 3;
+    public static final int HAND_CARD = 4;
+    public static final int DRAW_PILE_CARD = 5;
+    public static final int DISCARD_PILE_CARD = 6;
+    public static final int EXHAUST_PILE_CARD = 7;
+    public static final int MONSTER = 8;
+    public static final int POTION = 9;
+    public static final int SELECTION = 10;
+    public static final int MAP = 11;
+    public static final int REWARD = 12;
+    public static final int EVENT = 13;
+    public static final int SHOP = 14;
+    public static final int REST = 15;
+    public static final int SIZE = 16;
 
     private ProtocolSlots() {
     }
@@ -81,26 +85,73 @@ public final class ProtocolSlots {
     }
 
     /** Slots corresponding to {@code CardAccess}. */
-    public static final class Card {
-        public static final int MASTER_DECK = 0;
-        public static final int HAND = 1;
-        public static final int DISCARD_PILE = 2;
-        public static final int EXHAUST_PILE = 3;
-        public static final int CARD_WIRE_ID = 4;
-        public static final int TYPE = 5;
-        public static final int TARGET = 6;
-        public static final int RARITY = 7;
-        public static final int COST = 8;
-        public static final int COST_FOR_TURN = 9;
-        public static final int CHARGE_COST = 10;
-        public static final int PRICE = 11;
-        public static final int UPGRADED = 12;
-        public static final int IN_BOTTLE_FLAME = 13;
-        public static final int IN_BOTTLE_LIGHTNING = 14;
-        public static final int IN_BOTTLE_TORNADO = 15;
-        public static final int SIZE = 16;
+    public static final class DeckCard {
+        public static final int CARD_WIRE_ID = 0;
+        public static final int COST_FOR_TURN = 1;
+        public static final int PRICE = 2;
+        public static final int UPGRADED = 3;
+        public static final int IN_BOTTLE_FLAME = 4;
+        public static final int IN_BOTTLE_LIGHTNING = 5;
+        public static final int IN_BOTTLE_TORNADO = 6;
+        public static final int SIZE = 7;
 
-        private Card() {
+        private DeckCard() {
+        }
+    }
+
+    public static final class HandCard {
+        public static final int CARD_WIRE_ID = 0;
+        public static final int COST_FOR_TURN = 1;
+        public static final int PRICE = 2;
+        public static final int UPGRADED = 3;
+        public static final int IN_BOTTLE_FLAME = 4;
+        public static final int IN_BOTTLE_LIGHTNING = 5;
+        public static final int IN_BOTTLE_TORNADO = 6;
+        public static final int SIZE = 7;
+
+        private HandCard() {
+        }
+    }
+
+    public static final class DrawPileCard {
+        public static final int CARD_WIRE_ID = 0;
+        public static final int COST_FOR_TURN = 1;
+        public static final int PRICE = 2;
+        public static final int UPGRADED = 3;
+        public static final int IN_BOTTLE_FLAME = 4;
+        public static final int IN_BOTTLE_LIGHTNING = 5;
+        public static final int IN_BOTTLE_TORNADO = 6;
+        public static final int SIZE = 7;
+
+        private DrawPileCard() {
+        }
+    }
+
+    public static final class DiscardPileCard {
+        public static final int CARD_WIRE_ID = 0;
+        public static final int COST_FOR_TURN = 1;
+        public static final int PRICE = 2;
+        public static final int UPGRADED = 3;
+        public static final int IN_BOTTLE_FLAME = 4;
+        public static final int IN_BOTTLE_LIGHTNING = 5;
+        public static final int IN_BOTTLE_TORNADO = 6;
+        public static final int SIZE = 7;
+
+        private DiscardPileCard() {
+        }
+    }
+
+    public static final class ExhaustPileCard {
+        public static final int CARD_WIRE_ID = 0;
+        public static final int COST_FOR_TURN = 1;
+        public static final int PRICE = 2;
+        public static final int UPGRADED = 3;
+        public static final int IN_BOTTLE_FLAME = 4;
+        public static final int IN_BOTTLE_LIGHTNING = 5;
+        public static final int IN_BOTTLE_TORNADO = 6;
+        public static final int SIZE = 7;
+
+        private ExhaustPileCard() {
         }
     }
 
@@ -182,16 +233,15 @@ public final class ProtocolSlots {
 
     /** Slots corresponding to {@code ShopAccess}. */
     public static final class Shop {
-        public static final int COLORED_CARDS = 0;
-        public static final int COLORLESS_CARDS = 1;
-        public static final int POTIONS = 2;
-        public static final int PURGE_AVAILABLE = 3;
-        public static final int ACTUAL_PURGE_COST = 4;
-        public static final int RELIC = 5;
-        public static final int RELIC_PRICE = 6;
-        public static final int POTION = 7;
-        public static final int POTION_PRICE = 8;
-        public static final int SIZE = 9;
+        public static final int CARDS = 0;
+        public static final int CARDS_PRICE = 1;
+        public static final int PURGE_AVAILABLE = 2;
+        public static final int ACTUAL_PURGE_COST = 3;
+        public static final int RELIC = 4;
+        public static final int RELIC_PRICE = 5;
+        public static final int POTION = 6;
+        public static final int POTION_PRICE = 7;
+        public static final int SIZE = 8;
 
         private Shop() {
         }
